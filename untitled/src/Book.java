76a34fcbe365236a5f0ad4e8e0470fdcd6e3c366
avetkost <kostandyan0298@gmail.com>
2021-05-25 02:12:01 +0400

    gg
    gg

--- untitled/src/Book.java
+++ untitled/src/Book.java
@@ -1,3 +1,43 @@
 public class Book {
+    private String isbn,name;
+    private Author author;
+    private double price;
+    private int qty = 0;
 
+    public Book(String isbn,String name,Author author,double price,int qty){
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+    public String getISBN(){
+        return isbn;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAuthor(){
+        return author.toString();
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double newPrice){
+        this.price = newPrice;
+    }
+    public int getQty(){
+        return qty;
+    }
+    public  void setQty(int newQty){
+        this.qty = newQty;
+    }
+
+    public String getAuthorName() {
+        return author.getName();
+    }
+
+    public String toString(){
+        return "Book[isbn="+isbn+",name="+name+","+author.toString()+",price="+price+",qty="+qty+"]";
+    }
 }
--- untitled/src/main.java
+++ untitled/src/main.java
@@ -6,13 +6,15 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-    Author obj = new Author("valod","boxk@gmail.com");
+    Author author1 = new Author("valod","valod@gmail.com");
 
 
-        obj.setEmail("bazuk@gmail.com");
-        System.out.println(obj.getEmail());
-        System.out.println(obj.toString());
+//        obj.setEmail("bazuk@gmail.com");
+//        System.out.println(author1.getEmail());
+//        System.out.println(author1.toString());
+    Book book1 = new Book("Fight Club","Fight",author1,20000,60);
 
+        System.out.println(book1.getAuthor());
     }
 }
 
